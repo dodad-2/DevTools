eaf013ff5178e53ed239067205f1983b09731ddd
dodad-2 <github@lafon.us>
2024-01-18 23:09:36 -0600

    Spawn Unit

--- RebindCheats.cs
+++ RebindCheats.cs
@@ -1,6 +1,8 @@
 using QList.OptionTypes;
 using DevTools;
 using MelonLoader;
+using Il2Cpp;
+using UnityEngine;
 
 namespace DevTools.RebindCheats;
 
@@ -12,9 +14,11 @@ public static class Rebind
     private static KeybindOption? playPauseKeybind;
     private static KeybindOption? addTimeKeybind;
     private static KeybindOption? subtractTimeKeybind;
-
     private static bool keybindPaused;
     private static float previousTimeScale;
+
+    public static StringOption? spawnUnitString;
+    private static KeybindOption? spawnUnitKeybind;
 
     public static void CreateOptions()
     {
@@ -35,16 +39,21 @@ public static class Rebind
 
         subtractTimeKeybind = new QList.OptionTypes.KeybindOption(MelonPreferences.CreateEntry<string>(category.Identifier, "SUBTRACTTIME", "", "Subtract Time"));
         subtractTimeKeybind.OnKeybindDown += new Action<KeybindOption>(StartSubtractTime);
-        subtractTimeKeybind.OnKeybindUp += new Action<KeybindOption>(StopSubtractTime);
         QList.Options.AddOption(subtractTimeKeybind);
+        subtractTimeKeybind.OnKeybindUp += new Action<KeybindOption>(StopSubtractTime);
+
+        spawnUnitString = new StringOption();
+        QList.Options.AddOption(spawnUnitString, "Spawn Unit Name", null, "Cheat Assists");
+
+        spawnUnitKeybind = new KeybindOption(MelonPreferences.CreateEntry<string>(category.Identifier, "SPAWNUNITKEYBIND", "", "Spawn Unit Keybind"));
+        spawnUnitKeybind.OnKeybindDown += new Action<KeybindOption>(SpawnUnit);
+        QList.Options.AddOption(spawnUnitKeybind, "Spawn Unit Keybind", null, "Cheat Assists");
 
         category.SaveToFile();
     }
 
     public static void ToggleTime(KeybindOption option)
     {
-        Log.LogOutput($"Rebind.ToggleTime: {keybindPaused}");
-
         if (Il2Cpp.GameManager.Instance == null)
             return;
 
@@ -73,5 +82,31 @@ public static class Rebind
     public static void StopSubtractTime(KeybindOption option)
     {
         SubtractTime = false;
+    }
+    public static void SpawnUnit(KeybindOption option)
+    {
+        if (spawnUnitString == null)
+            return;
+
+        var prefabIndex = GameDatabase.GetSpawnablePrefabIndex(spawnUnitString.GetValue());
+
+        if (prefabIndex < 0)
+            return;
+
+        var prefab = GameDatabase.GetSpawnablePrefab(prefabIndex);
+
+        var positionRay = Game.CurrentCamera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+        Vector3 position = new Vector3(0, 2f, 0);
+
+        RaycastHit hitInfo;
+
+        if (UnityEngine.Physics.Raycast(positionRay, out hitInfo, Mathf.Infinity))
+        {
+            position += hitInfo.point;
+
+            var cross = Vector3.Cross(Game.CurrentCamera.transform.right, Vector3.up);
+
+            Game.SpawnPrefab(prefab, null, Player.CurrentPlayer.Team, position, Quaternion.LookRotation(cross), true, true);
+        }
     }
 }
